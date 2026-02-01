751512d646c3ad17afa595e0d7624376161e001f
Iziny <yoanndewilde@gmail.com>
2026-02-01 13:11:15 +0100

    ai behaviors wip

--- Assets/Scripts/AI/FollowTarget.cs
+++ Assets/Scripts/AI/FollowTarget.cs
@@ -1,26 +1,86 @@
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum AgentBehavior
+{
+    AttackPlayer,
+    Flee,
+    StopVentilator,
+}
+
 [RequireComponent(typeof(NavMeshAgent))]
 public class FollowTarget : MonoBehaviour
 {
-    public Transform target;
+    Transform target;
+    public Transform ventilator;
+    public Transform[] randomRooms;
     public float MinDistance = 1.5f;
     public float MaxDistance = 10f;
+    public AgentBehavior behavior;
     NavMeshAgent agent;
+
+    public float stoppingVentilatorDelay = 10;
+    private float stoppingVentilatorTime;
 
     void Start()
     { 
         agent = GetComponent<NavMeshAgent>();
         agent.stoppingDistance = MinDistance;
+        stoppingVentilatorTime = stoppingVentilatorDelay;
 
-        if (target == null)
-            target = FindFirstObjectByType<IsoTPSController>().transform;
+        switch (behavior)
+        {
+
+            case AgentBehavior.AttackPlayer:
+                target = FindFirstObjectByType<IsoTPSController>().transform;
+                break;
+
+            case AgentBehavior.StopVentilator:
+                target = ventilator;
+                break;
+
+            case AgentBehavior.Flee:
+                target = randomRooms[Random.Range(0, randomRooms.Length)];
+                break;
+        }
     }
 
     void Update()
     {
-        if (target != null)
-            agent.SetDestination(target.position);
+        agent.SetDestination(target.position);
+        gameObject.transform.LookAt(target.position);
+
+        switch (behavior)
+        {
+
+            case AgentBehavior.AttackPlayer:
+                if (agent.remainingDistance == MinDistance)
+                {
+
+                }
+                break;
+
+            case AgentBehavior.StopVentilator:
+                target = ventilator;
+                if (agent.remainingDistance <= MinDistance + 1.5f)
+                {
+                    stoppingVentilatorTime -= Time.deltaTime;
+                    Debug.Log($"Killing {stoppingVentilatorTime} sec");
+                } else
+                {
+                    //stoppingVentilatorTime = stoppingVentilatorDelay;
+                }
+                break;
+
+            case AgentBehavior.Flee:
+                target = randomRooms[Random.Range(0, randomRooms.Length)];
+
+                break;
+        }
+
+        if (stoppingVentilatorTime <= 0)
+        {
+            Debug.Log("YOU DIED");
+        }
     }
 }
